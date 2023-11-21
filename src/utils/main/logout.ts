ef5e7df87d7a6fa1cb128f6216637c87bf579361
M7ilan <distny2004@gmail.com>
2023-11-21 07:25:09 +0300

    Remove removeTokens function and update logout functionality

--- src/utils/main/logout.ts
+++ src/utils/main/logout.ts
@@ -1,11 +1,10 @@
-import removeTokens from "./removeTokens";
-
 export default async function Logout(): Promise<boolean> {
 	const response = await fetch("/api/auth/logout", { method: "POST" });
 	const { success } = await response.json();
 
 	if (success) {
-		removeTokens();
+		localStorage.removeItem("Auth");
+		localStorage.removeItem("User");
 		return true;
 	}
 	return false;
