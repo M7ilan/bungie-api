4ede73931cbe3f8fcd648ca97b4796d3551d1298
M7ilan <distny2004@gmail.com>
2023-11-22 02:08:22 +0300

    Add Discord link

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
-import { RiGithubFill } from "react-icons/ri";
+import { RiDiscordFill, RiGithubFill } from "react-icons/ri";
 
 export default function Socials({ size }: { size?: number }) {
 	const customSize = `${size || "32"}px`;
 
 	return (
-		<div className="flex gap-2 text-left items-center">
+		<div className="flex gap-4 text-left items-center">
 			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://github.com/m7ilan/bungie-api">
 				<RiGithubFill style={{ height: customSize, width: customSize }} />
+			</Link>
+			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://discord.gg/86CAxYJ6NK">
+				<RiDiscordFill style={{ height: customSize, width: customSize }} />
 			</Link>
 		</div>
 	);
