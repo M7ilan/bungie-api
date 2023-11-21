b66dc8d7b399090ec377ef4cc34770cde552eddf
M7ilan <distny2004@gmail.com>
2023-11-21 07:22:33 +0300

    Refactor page component and add loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { useEffect } from "react";
-import checkTokens from "../utils/main/checkTokens";
+import { useEffect, useState } from "react";
+import checkTokens from "@@/utils/main/checkTokens";
 import { useRouter } from "next/navigation";
-import Socials from "../components/Socials";
+import Socials from "@@/components/Socials";
+import LoadingIcon from "../components/LoadingIcon";
+import clsx from "clsx";
 
 export default function HomePage() {
 	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
+	const [uniqueName, setUniqueName] = useState("");
 
 	useEffect(() => {
 		(async () => {
@@ -18,13 +22,26 @@ export default function HomePage() {
 				}, 100);
 			}
 		})();
-	}, []);
 
+		const User = localStorage.getItem("User");
+		if (User) {
+			const { uniqueName } = JSON.parse(User);
+			setUniqueName(uniqueName);
+		}
+
+		setIsLoading(false);
+	}, []);
 	return (
-		<div className="flex flex-col gap-8 center">
-			<div className="header">You&apos;re in the App Page</div>
-			<div>Think about participating in the creation of the optimal Bungie API template.</div>
-			<Socials size={64} />
-		</div>
+		<>
+			<LoadingIcon classname={`${!isLoading && "opacity-0 pointer-events-none"}`} />
+			<div className={clsx("flex flex-col gap-12 center", { "opacity-0 pointer-events-none": isLoading })}>
+				<div className="flex flex-col gap-2">
+					<div className="header">Welcome {uniqueName}</div>
+					<div className="header">You&apos;re in the App Page</div>
+				</div>
+				<div>Think about participating in the creation of the optimal Bungie API template.</div>
+				<Socials size={64} />
+			</div>
+		</>
 	);
 }
