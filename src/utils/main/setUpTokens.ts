ff888834b4e1d036d0eba81f8ff2e1bd9507cf8d
M7ilan <distny2004@gmail.com>
2023-11-21 22:44:31 +0300

    Add async/await to setUpTokens function

--- src/utils/main/setUpTokens.ts
+++ src/utils/main/setUpTokens.ts
@@ -3,7 +3,10 @@
  * @param tokens - The tokens to be set up.
  * @returns True if the tokens were successfully set up in local storage, false otherwise.
  */
-export default function setUpTokens(tokens: TokensType): boolean {
+export default async function setUpTokens(code: string): Promise<boolean> {
+	const response1 = await fetch("/api/auth/get-tokens", { method: "POST", body: JSON.stringify({ code }) });
+	const tokens = await response1.json();
+
 	const newTokens = {
 		access_token: {
 			value: tokens.access_token,
