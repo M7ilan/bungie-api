7843a25c405aa36262788a362d39e212ca5f006c
M7ilan <distny2004@gmail.com>
2023-11-21 07:23:08 +0300

    Add Bungie API integration and set up user

--- src/app/get-code/page.tsx
+++ src/app/get-code/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import fetchBungie from "@/src/utils/bungie/fetchBungie";
+import setUpUser from "@/src/utils/bungie/setUpUser";
 import setUpTokens from "@/src/utils/main/setUpTokens";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
@@ -10,14 +12,23 @@ export default function GetCode() {
 	useEffect(() => {
 		if (code) {
 			(async () => {
-				const response = await fetch("/api/auth/get-tokens", { method: "POST", body: JSON.stringify({ code }) });
-				const json = await response.json();
-				if (response.ok) {
+				const response1 = await fetch("/api/auth/get-tokens", { method: "POST", body: JSON.stringify({ code }) });
+				const json = await response1.json();
+				if (response1.ok) {
 					setUpTokens(json);
-					setTimeout(() => {
-						location.href = "/";
-					}, 100);
 				}
+
+				const auth = localStorage.getItem("Auth");
+				if (!auth) return;
+				const newAuth: NewTokensType = JSON.parse(auth);
+				const access_token = newAuth.access_token.value;
+				const response2 = await fetchBungie("/api/bungie/User/GetMembershipsForCurrentUser", { headers: { access_token } });
+				const json2: GetMembershipsForCurrentUserType = await response2.json();
+				if (response2.ok) {
+					setUpUser(json2);
+				}
+
+				location.href = "/";
 			})();
 		}
 	}, []);
