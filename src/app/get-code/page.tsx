c69d7db09947d91bdcb1f767d54358583de90d7b
M7ilan <distny2004@gmail.com>
2023-11-21 22:44:08 +0300

    Refactor code to improve token setup and user setup

--- src/app/get-code/page.tsx
+++ src/app/get-code/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import LoadingIcon from "@/src/components/LoadingIcon";
-import fetchBungie from "@/src/utils/bungie/fetchBungie";
 import setUpUser from "@/src/utils/bungie/setUpUser";
 import setUpTokens from "@/src/utils/main/setUpTokens";
 import { useSearchParams } from "next/navigation";
@@ -14,20 +13,14 @@ export default function GetCode() {
 	useEffect(() => {
 		if (code) {
 			(async () => {
-				const response1 = await fetch("/api/auth/get-tokens", { method: "POST", body: JSON.stringify({ code }) });
-				const json = await response1.json();
-				if (response1.ok) {
-					setUpTokens(json);
+				const response1 = await setUpTokens(code);
+				if (response1) {
+					console.log("Successfully set up tokens");
 				}
 
-				const auth = localStorage.getItem("Auth");
-				if (!auth) return;
-				const newAuth: NewTokensType = JSON.parse(auth);
-				const access_token = newAuth.access_token.value;
-				const response2 = await fetchBungie("/api/bungie/User/GetMembershipsForCurrentUser", { headers: { access_token } });
-				const json2: GetMembershipsForCurrentUserType = await response2.json();
-				if (response2.ok) {
-					setUpUser(json2);
+				const response2 = await setUpUser();
+				if (response2) {
+					console.log("Successfully set up user");
 				}
 
 				setIsLoading(false);
@@ -36,5 +29,10 @@ export default function GetCode() {
 		}
 	}, []);
 
-	if (isLoading) return <div className="header center gap-4">Logging in... <LoadingIcon /></div>;
+	if (isLoading)
+		return (
+			<div className="header center gap-4">
+				Logging in... <LoadingIcon />
+			</div>
+		);
 }
